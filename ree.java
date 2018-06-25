34edc0daba6fdf997e317b7597fb528e4f64fb52
Garatrope <30671349+Garatrope@users.noreply.github.com>
2018-06-26 09:52:15 +1000

    Create ree.java

---
+++ ree.java
@@ -0,0 +1,11 @@
+class compatability {
+
+}
+
+public void similar(int ) {
+System.out.println("This planets compatibilty rate is " + similar + " percent"
+}
+
+public static void main(String[] args) {
+
+}
